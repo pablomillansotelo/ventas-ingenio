e1fc192867750ee870d648556b364d32cb930a77
Juan Pablo Millan Sotelo <pablomillansotelo@gmail.com>
2025-08-01 00:21:46 +0000

    fix: staticfiles

--- ventas-django/api/settings.py
+++ ventas-django/api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i^s2bj(gflcf5cza9go9#3-_z1&4bpksf*nas01ukb_p)cj2ti'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'blog.juampamillan.com', '.github.dev', 'localhost']
 
 
 # Application definition
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'widget_tweaks',
     'ventas',
 
 ]
@@ -80,16 +80,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'sql_server.pyodbc',
-        'NAME': 'puntodeventa',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'INSPIRON-3458\SQLEXPRESS',
-        'port': '1433',
-
-        'OPTIONS': {
-            'driver': 'ODBC Driver 17 for SQL Server',
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
@@ -129,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directorio donde collectstatic recopila los archivos
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Directorio donde guardas tus archivos estáticos en desarrollo
 
 #media urls
 MEDIA_URL = '/media/'
--- ventas-django/api/urls.py
+++ ventas-django/api/urls.py
@@ -22,7 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ventas.urls')),
 
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
